efd82990d4b079ee7b9b015690137ced4b3b94d0
Victor Bianchi <vbianchi90@gmail.com>
2018-06-14 22:14:43 -0400

    implemented map with hard-coded data

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './marker'
 
 class Map extends Component {
   // constructor(props) = {
@@ -8,8 +10,29 @@ class Map extends Component {
   //   }
   // }
 
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
-    return null
+    return (
+      <div className='map-container'>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAK2GxhKdB8sU5su4E-nYZ7q2qSYblnKjQ' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <Marker
+            lat={59.955413}
+            lng={30.337844}
+          />
+        </GoogleMapReact>
+      </div>
+    )
   }
 }
 
